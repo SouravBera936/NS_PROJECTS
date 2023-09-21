7acfa25553a81e480de97239ddf3837735d61749
Sourav Bera <133385958+SouravBera936@users.noreply.github.com>
2023-09-21 12:12:31 +0530

    JAVASCRIPT

---
+++ JAVASCRIPT/ASSIGNMENTS/Find the value.js
@@ -0,0 +1,5 @@
+/*Create an array named arr which contains the first 5 multiples of 2. 
+After this create a variable named val which stores the middle element of the array.*/
+const arr=[2,4,6,8,10]
+var val=arr[2]
+console.log(val)
