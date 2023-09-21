d597ff59e0e6d11b0c6b59c3d279445522696caf
Sourav Bera <133385958+SouravBera936@users.noreply.github.com>
2023-09-21 12:09:47 +0530

    JAVASCRIPT

---
+++ JAVASCRIPT/ASSIGNMENTS/Create my Car.js
@@ -0,0 +1,10 @@
+/*You need to create an Object in Javascript with the name Car.
+This object will have three properties with name color, seats, nitros
+The table given below contains the properties of the object, their data type, and the value for that property.*/
+const Car={
+    color:"white",
+    seats:2,
+    nitros:false
+}
+const msg=`my car have ${Car.seats} seats and it is of ${Car.color} color`
+console.log(msg)
