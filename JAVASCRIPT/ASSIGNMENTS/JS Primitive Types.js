d61fdde6c67a3c02ee38a6e8dad85731c3cfd884
Sourav Bera <133385958+SouravBera936@users.noreply.github.com>
2023-09-21 12:17:16 +0530

    JAVASCRIPT

---
+++ JAVASCRIPT/ASSIGNMENTS/JS Primitive Types.js
@@ -0,0 +1,15 @@
+/*Create 6 variables with namesvarA, varB, varC, varD, varE, varF
+Store 6 different values of your choice in the variables such that the variables represent the given primitive types in the given order:
+varA: Number
+varB: String
+varC: Boolean
+varD: Undefined
+varE: Object
+varF: Symbol*/
+var varA=42;
+var varB="Sourav";
+var varC=false;
+var varD=undefined;
+var varE={key:"value"};
+var varF=Symbol("unique");
+console.log(varA,varB,varC,varD,varE,varF)
