753bca8b190295136e95fce6bcc68add1723b243
Sourav Bera <133385958+SouravBera936@users.noreply.github.com>
2023-10-19 01:04:05 +0530

    BOP

---
+++ BASICS OF PROGRAMMING/Simple Sum/src/Main.java
@@ -0,0 +1,13 @@
+/*Complete the function simple_sum() which takes in three numbers num1,
+num2, and num3, and prints out the sum of these three numbers.
+ */
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int A=sc.nextInt();
+        int B=sc.nextInt();
+        int C=sc.nextInt();
+        System.out.println(A+B+C);
+    }
+}
